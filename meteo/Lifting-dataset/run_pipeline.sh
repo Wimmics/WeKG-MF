f0c7d5fd43cba09b951d571a449b8dfc9f576386
NadiaYA2019 <nadia.yacoubi.ayadi@gmail.com>
2021-12-09 05:41:18 +0100

    Update run_pipeline.sh

--- meteo/Lifting-dataset/run_pipeline.sh
+++ meteo/Lifting-dataset/run_pipeline.sh
@@ -9,12 +9,20 @@
 # Inspired from scripts written by Franck Michel (https://github.com/frmichel)
 # Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
  
- . ../Lifting-dataset/env.sh
+ . ./env.sh
 
 log() {
     echo "[$(date '+%F %T')] $1"
 }
 
+help()
+{
+  exe=$(basename $0)
+  echo "Usage: $exe <MongoDB collection> <Path Directory> <xR2RML mapping> <output file name>"
+  echo "Example:"
+  echo "   $exe  ColObservations2021  /Users/nyacaoub/Documents/Github/d2kab/meteo/Lifting-dataset/raw-weather-data/  mapping_observation.ttl  observations.ttl"
+  exit 1
+}
 
 echo "==========================================================================="
 
@@ -25,9 +33,14 @@ if [[ -z "$collection" ]] ; then help; fi
 dirpath=$2
 if [[ -z "$dirpath" ]] ; then help; fi
 
-python3 preprocess.py $collection $dirpath
+log "Preprocessing and Importing files into MongoDB..."
 
-cd xr2rml 
+python3 script.py $collection $dirpath
+
+
+log "Generating RDF files..."
+
+cd xr2rml
 
 mappingTemplate=$3
 if [[ -z "$mappingTemplate" ]] ; then help; fi
@@ -35,5 +48,5 @@ if [[ -z "$mappingTemplate" ]] ; then help; fi
 output=$4
 if [[ -z "$output" ]] ; then help; fi
 
-. ./run_xr2rml.sh $collection $mappingTemplate $output 
+./run_xr2rml.sh $collection $mappingTemplate $output 
 
