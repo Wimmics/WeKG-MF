12207709a953e8337709f5298b72fce2e2831b9b
NadiaYA2019 <60549124+NadiaYA2019@users.noreply.github.com>
2022-02-17 17:11:34 +0100

    new ENV.sh

---
+++ meteo/Lifting-dataset/ENV.sh
@@ -0,0 +1,5 @@
+export URL="https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop"
+
+export DIR="/Users/nyacaoub/Documents/Meteo-France/"
+
+export DATASET_DIR="/Users/nyacaoub/Documents/GitHub/d2kab/meteo/dataset/"
